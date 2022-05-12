38d5b53cfb8cd25ba66014903b66da1439e52b0a
Yicai Yu <yuyicai@hotmail.com>
2022-05-12 10:53:39 +0800

    ci: fix gin-vue test
    Signed-off-by: Yicai Yu <yuyicai@hotmail.com>

--- official-stack/gin-vue/test/repo_test.go
+++ official-stack/gin-vue/test/repo_test.go
@@ -5,7 +5,8 @@ import (
 	"os"
 	"strings"
 
-	"github.com/google/go-github/github"
+	"github.com/google/go-github/v43/github"
+	"golang.org/x/oauth2"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -16,14 +17,37 @@ var _ = Describe("Repos", func() {
 	})
 
 	It("should create backend, frontend, deploy repos", func() {
-		client := github.NewClient(nil)
 
 		githubOrg := os.Getenv("ORGANIZATION")
 		appName := os.Getenv("APP_NAME")
+		githubToken := os.Getenv("GITHUB_TOKEN")
 
-		// list public repositories for given org
-		opt := &github.RepositoryListByOrgOptions{Type: "public"}
-		repos, _, err := client.Repositories.ListByOrg(context.Background(), githubOrg, opt)
+		ctx := context.Background()
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: githubToken},
+		)
+		tc := oauth2.NewClient(ctx, ts)
+
+		client := github.NewClient(tc)
+		user, _, err := client.Users.Get(ctx, "")
+
+		Expect(err).To(BeNil())
+
+		userName := *user.Login
+
+		var repos []*github.Repository
+
+		// list all repositories
+		if userName == githubOrg {
+			// list all repositories for the authenticated user
+			opt := &github.RepositoryListOptions{Type: "all", Sort: "created"}
+			repos, _, err = client.Repositories.List(ctx, "", opt)
+		} else {
+			// list public repositories for given org
+			opt := &github.RepositoryListByOrgOptions{Type: "all", Sort: "created"}
+			repos, _, err = client.Repositories.ListByOrg(ctx, githubOrg, opt)
+		}
+
 		Expect(err).To(BeNil())
 
 		// find repo with app name prefix
@@ -36,11 +60,11 @@ var _ = Describe("Repos", func() {
 				continue
 			}
 			switch rn {
-			case appName + "backend":
+			case appName + "-backend":
 				foundBackend = true
-			case appName + "frontend":
+			case appName + "-frontend":
 				foundFrontend = true
-			case appName + "deploy":
+			case appName + "-deploy":
 				foundDeploy = true
 			}
 		}
