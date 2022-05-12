41b9a43b76f09d27d7d2b2754336f59a8759652f
xinxin huang <2011150377@email.szu.edu.cn>
2022-05-12 14:18:09 +0800

    mark some components as infra

--- chain/components/addons/nocalhost/copy.sh
+++ chain/components/addons/nocalhost/copy.sh
@@ -13,5 +13,5 @@ yq -i '.service.type = "ClusterIP"' "/scaffold/${OUTPUT_PATH}/infra/nocalhost/va
 # echo '{"username": "admin", "password": "123456", "url":"$NOCALHOST_DOMAIN"}' > "/scaffold/${OUTPUT_PATH}/infra/nocalhost-cd-output-hook.txt"
 
 cat <<EOF > /scaffold/${OUTPUT_PATH}/infra/nocalhost-cd-output-hook.txt
-{"username": "admin", "password": "123456", "url":"$NOCALHOST_DOMAIN"}
+{"username": "admin", "password": "123456", "url":"$NOCALHOST_DOMAIN", "infra": "true"}
 EOF
