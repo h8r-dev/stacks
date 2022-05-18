902b4adeb16ca72764b5ab43d3add3f107223923
xinxinh <2011150377@email.szu.edu.cn>
2022-05-18 14:12:34 +0800

    change infra type (#219)

--- chain/components/addons/loki/copy.sh
+++ chain/components/addons/loki/copy.sh
@@ -12,5 +12,5 @@ tar -zxvf ./loki-stack-$VERSION.tgz -C /scaffold/$OUTPUT_PATH/infra
 mv /scaffold/$OUTPUT_PATH/infra/loki-stack /scaffold/$OUTPUT_PATH/infra/loki
 
 cat <<EOF > /scaffold/${OUTPUT_PATH}/infra/loki-cd-output-hook.txt
-{"infra": "true"}
+{"infra": true}
 EOF
