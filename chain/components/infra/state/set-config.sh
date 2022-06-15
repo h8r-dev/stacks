3f19b95f61fd7e37b232b17e36785a8ef6279858
Yicai Yu <yuyicai@hotmail.com>
2022-06-15 13:58:54 +0800

    fix: lookup argocd-repo-server no such host
    Signed-off-by: Yicai Yu <yuyicai@hotmail.com>

--- chain/components/infra/state/set-config.sh
+++ chain/components/infra/state/set-config.sh
@@ -2,8 +2,10 @@
 
 # argocd
 echo "set argocd config"
-argocd_password=$(kubectl -n "${NAMESPACE}" get secret -n heighliner-infra argocd-initial-admin-secret -o=jsonpath='{.data.password}'|base64 -d)
-argocd_url="argo-argocd-server.${NAMESPACE}.svc"
+# argocd_password=$(kubectl -n "${NAMESPACE}" get secret -n heighliner-infra argocd-initial-admin-secret -o=jsonpath='{.data.password}'|base64 -d)
+# argocd_url="argo-argocd-server.${NAMESPACE}.svc"
+argocd_password=$(kubectl -n argocd get secret -n heighliner-infra argocd-initial-admin-secret -o=jsonpath='{.data.password}'|base64 -d)
+argocd_url="argocd-server.argocd.svc"
 
 yq -i '.argocd.url = "'"$argocd_url"'"' config.yaml
 yq -i '.argocd.credentials.username = "admin"' config.yaml
