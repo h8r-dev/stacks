d3e1e1076482be70f94f6f1d508fe7aa31b21098
wangwei <434533508@qq.com>
2022-06-15 14:57:23 +0800

    fix: argocd namespace
    Signed-off-by: wangwei <434533508@qq.com>

--- chain/components/infra/state/set-config.sh
+++ chain/components/infra/state/set-config.sh
@@ -4,7 +4,7 @@
 echo "set argocd config"
 # argocd_password=$(kubectl -n "${NAMESPACE}" get secret -n heighliner-infra argocd-initial-admin-secret -o=jsonpath='{.data.password}'|base64 -d)
 # argocd_url="argo-argocd-server.${NAMESPACE}.svc"
-argocd_password=$(kubectl -n argocd get secret -n heighliner-infra argocd-initial-admin-secret -o=jsonpath='{.data.password}'|base64 -d)
+argocd_password=$(kubectl -n argocd get secret argocd-initial-admin-secret -o=jsonpath='{.data.password}'|base64 -d)
 argocd_url="argocd-server.argocd.svc"
 
 yq -i '.argocd.url = "'"$argocd_url"'"' config.yaml
