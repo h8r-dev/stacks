8af44c9dc160fa769dfc786e105a29be2996e25e
Jinming Guo <vgjm@foxmail.com>
2022-07-15 17:28:55 +0800

    fix: detect if push is needed when rerun stack (#377)
    Signed-off-by: vgjm <vgjm@foxmail.com>

--- chain/v4/pkg/git/push.sh
+++ chain/v4/pkg/git/push.sh
@@ -5,6 +5,13 @@ git config user.email "bot@forkmain.com"
 
 git add --all
 
+if [ -n "$(git status --porcelain)" ]; then
+  echo "there are changes";
+else
+  echo "no changes";
+  exit 0
+fi
+
 git commit -m "forkmain: set up this repo"
 git remote add forkmain-origin https://${TOKEN}@github.com/${ORGANIZATION}/${NAME}.git
 git push forkmain-origin main
