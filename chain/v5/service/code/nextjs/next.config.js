66a5e2e4fa3bb4915a3eef3d8638c714da807fee
vgjm <vgjm@foxmail.com>
2022-08-05 20:41:03 +0800

    fix: update nextjs codes
    Signed-off-by: vgjm <vgjm@foxmail.com>

--- chain/v5/service/code/nextjs/next.config.js
+++ chain/v5/service/code/nextjs/next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    outputStandalone: true,
-  },
+  swcMinify: true,
 }
 
 module.exports = nextConfig
