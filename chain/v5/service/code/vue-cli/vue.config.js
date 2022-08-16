1fe56360b3a787d2a127124282e7b1f08eea48cc
zhangze <1183460943@qq.com>
2022-08-16 14:25:45 +0800

    fix vue-cli allowhost

--- chain/v5/service/code/vue-cli/vue.config.js
+++ chain/v5/service/code/vue-cli/vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 3000
+    port: 3000,
+    allowedHosts: "all"
   }
 })
